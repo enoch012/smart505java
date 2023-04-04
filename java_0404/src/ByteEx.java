3d284a60947073f8bd90eb385a61f90459fce49c
enoch012 <zzan6238@naver.com>
2023-04-04 16:09:13 +0900

    Byte 타입 실습 / 최대범위

---
+++ java_0404/src/ByteEx.java
@@ -0,0 +1,19 @@
+public class ByteEx {
+
+  public static void main(String[] args){
+    byte var1 = -128;
+    byte var2 = 127;
+    byte var3 = 0;
+    byte var4 = 30;
+    byte var5 = -30;
+
+    // byte 타입이 최소 -128 ~ 최대 127까지 저장가능한데 최대 범위가 넘어가면 오류가 남
+    // byte var6 = 128;
+
+    System.out.println(var1);
+    System.out.println(var2);
+    System.out.println(var3);
+    System.out.println(var4);
+    System.out.println(var5);
+  }
+}
