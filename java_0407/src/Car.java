dc9ce355a057f3e7857ba166e7ae387fb78bdea9
enoch012 <zzan6238@naver.com>
2023-04-10 17:01:42 +0900

    Car 클래스 생성 및 사용 기본 예제

---
+++ java_0407/src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+
+  // 필드
+  String company = "쌍용자동차"; // 필드 선언과 동시에 초기값 설정
+  String model = "토레스";
+  String color = "회색";
+  int maxSpeed = 250;
+  int speed; // 필드를 선언만 함
+}
+// 빌드하면 클래스별로 나눠져서 저장 된다.
+//class Tire {}
