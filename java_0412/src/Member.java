d24ae9da1faab9b11105100a0c0ee9d0df1b0db9
enoch012 <zzan6238@naver.com>
2023-04-12 17:14:32 +0900

    final 클래스와 상속 / 상속을 받을 수는 있다

---
+++ java_0412/src/Member.java
@@ -0,0 +1,4 @@
+public final class Member {
+  // final 메소드 테스트 클래스
+  // class에 final을 붙이면 다른 클래스가 상속 할 수 없다. 상속 받을 수는 있음.
+}
---
+++ java_0412/src/VIPMember.java
@@ -0,0 +1,4 @@
+/*클래스에 final을 붙이면 상속이 불가능해진다.*/
+public class VIPMember /*extends Member*/ {
+
+}
