3901890a6b8bd5833fbda1bc3fcd4d240a6d98e2
enoch012 <zzan6238@naver.com>
2023-04-20 09:32:27 +0900

    Vector 예시 / 데이터 입력 삭제 속도가 더 빠른 벡터 활용 (활용법은 ArrayList와 거의 똑같다)

---
+++ java_0418/src/pr04_collection_Vector/Board.java
@@ -0,0 +1,15 @@
+package pr04_collection_Vector;
+
+/* Vector 활용 예시를 위한 클래스 생성 */
+
+public class Board {
+  String subject;
+  String content;
+  String writer;
+
+  public Board(String subject, String content, String writer){
+    this.subject = subject;
+    this.content = content;
+    this.writer = writer;
+  }
+}
